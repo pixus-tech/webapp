53bae73725e09943a98a8e0ad9cc3cb606135147
Tom König <hi@tomknig.de>
2020-01-09 12:07:25 +0100

    Get rid of analytics

--- src/containers/gates/AnalyticsGate.tsx
+++ src/containers/gates/AnalyticsGate.tsx
@@ -38,19 +38,7 @@ class AnalyticsGate extends React.PureComponent<ComposedProps> {
       if (this.props.optOutAnalytics === false) {
         ReactDOM.render(
           <>
-            {`(function(f, a, t, h, o, m){
-                a[h]=a[h]||function(){
-                  (a[h].q=a[h].q||[]).push(arguments)
-                };
-                o=f.createElement('script'),
-                  m=f.getElementsByTagName('script')[0];
-                o.async=1; o.src=t; o.id='fathom-script';
-                m.parentNode.insertBefore(o,m)
-              })(document, window, '//cdn.pixus.tech/tracker.js', 'fathom');
-              fathom('set', 'siteId', 'WPOSXCUV');
-              /* fathom('trackPageview'); */
-              /* fathom('set', 'spa', 'pushstate'); */
-            `}
+            {``}
           </>,
           scriptRef,
         )
