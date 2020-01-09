fbf7b4f167a69ca666564232d989965181e4fa72
Tom König <hi@tomknig.de>
2020-01-09 14:47:02 +0100

    Use info log-level in production

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,8 @@ import '@formatjs/intl-relativetimeformat/polyfill'
 import '@formatjs/intl-relativetimeformat/dist/locale-data/en'
 import '@formatjs/intl-relativetimeformat/dist/locale-data/zh'
 
+import { config } from 'blockstack'
+
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 
@@ -20,6 +22,10 @@ import { Provider } from 'react-redux'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import storeConfiguration from './store'
+
+if (process.env.NODE_ENV === 'production') {
+  config.logLevel = 'info'
+}
 
 const Root = () => (
   <MuiThemeProvider theme={theme}>
